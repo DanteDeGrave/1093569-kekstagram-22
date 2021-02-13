6e2cc9cf60f65a2a7811041062c38cdc93678bd9
DanteDeGrave <vladimir.system@gmail.com>
2021-02-13 13:40:12 +0500

    Оптимизировал код и исправил косяк-избыточные действия постоянного поиска элемента по дом, при вызове в цикле функции

--- js/picture-generator.js
+++ js/picture-generator.js
@@ -3,23 +3,15 @@ import {getArrayObjects} from './object-creator.js';
 const photosList = document.querySelector('.pictures');
 const photos = getArrayObjects();
 
-const getPictureElement = (object) => {
-  const templatePicture = document.querySelector('#picture').content;
-  const picture = templatePicture.cloneNode(true);
-
-  picture.querySelector('.picture__img').src = object.url;
-  picture.querySelector('.picture__comments').textContent = String(object.comments.length);
-  picture.querySelector('.picture__likes').textContent = String(object.likes);
-
-  return picture;
-}
-
-
-
 const getFragmentsPicture = (objects) => {
+  const templatePicture = document.querySelector('#picture').content;
   const fragmentsPicture = document.createDocumentFragment();
-  objects.forEach((item) => {
-    fragmentsPicture.appendChild(getPictureElement(item));
+  objects.forEach((element) => {
+    const picture = templatePicture.cloneNode(true);
+    picture.querySelector('.picture__img').src = element.url
+    picture.querySelector('.picture__comments').textContent = String(element.comments.length);
+    picture.querySelector('.picture__likes').textContent = String(element.likes);
+    fragmentsPicture.appendChild(picture);
   });
   return fragmentsPicture;
 }
