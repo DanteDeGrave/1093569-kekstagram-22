4e4cffffa382967c7ae9be5ebc259efda6b358f3
DanteDeGrave <vladimir.system@gmail.com>
2021-02-13 10:43:44 +0500

    Выполнил первое задание 5ого блока

---
+++ js/picture-generator.js
@@ -0,0 +1,29 @@
+import {getArrayObjects} from './object-creator.js';
+
+const photosList = document.querySelector('.pictures');
+const photos = getArrayObjects();
+
+const getPictureElement = (object) => {
+  const templatePicture = document.querySelector('#picture').content;
+  const picture = templatePicture.cloneNode(true);
+  const pictureAddress = picture.querySelector('.picture__img');
+  const pictureLikes = picture.querySelector('.picture__likes');
+  const pictureComments = picture.querySelector('.picture__comments');
+
+  pictureAddress.src = object.url;
+  pictureComments.textContent = String(object.comments.length);
+  pictureLikes.textContent = String(object.likes);
+
+  return picture;
+}
+
+const getFragmentsPicture = (objects) => {
+  const fragmentsPicture = document.createDocumentFragment();
+  for (let i = 0; i < objects.length; i++) {
+    fragmentsPicture.appendChild(getPictureElement(objects[i]));
+  }
+  return fragmentsPicture;
+}
+
+photosList.appendChild(getFragmentsPicture(photos));
+
