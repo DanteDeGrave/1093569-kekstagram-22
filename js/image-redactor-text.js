bdbb1c88f701c737264b2af0ec33ac0caf8bea92
DanteDeGrave <vladimir.system@gmail.com>
2021-02-21 23:11:35 +0500

    Убрал очипятки

--- js/image-redactor-text.js
+++ js/image-redactor-text.js
@@ -8,9 +8,7 @@ const HASHTAG_MAX_LENGTH = 20;
 const HASHTAG_MAX_COUNT = 5;
 
 const validationHashtag = () => {
-  let arrayHashTags = [];
-
-  arrayHashTags = hashtagInput.value.toLowerCase().split(' ');
+  let arrayHashTags = hashtagInput.value.toLowerCase().split(' ');
 
   arrayHashTags.forEach((element, index,array)=>{
     if (element.length >= HASHTAG_MAX_LENGTH) {
@@ -30,7 +28,7 @@ const validationHashtag = () => {
 
 const validationDescription = () => {
   if (validationStringLength(description.value, DESCRIPTION_MAX_LENGTH)) {
-    description.setCustomValidity(`Длинна комментария не должна превышать ${DESCRIPTION_MAX_LENGTH} символов`);;
+    description.setCustomValidity(`Длинна комментария не должна превышать ${DESCRIPTION_MAX_LENGTH} символов`);
   } else {
     description.setCustomValidity('');
   }
