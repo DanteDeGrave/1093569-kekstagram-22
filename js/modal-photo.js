615d99775bace978241241b5bc220a1bcf2bc884
DanteDeGrave <vladimir.system@gmail.com>
2021-03-03 01:08:59 +0500

    реализация кнопки 'показать еще', но есть ошибки

--- js/modal-photo.js
+++ js/modal-photo.js
@@ -7,6 +7,9 @@ const modalPhotoDescription = modalPhoto.querySelector('.social__caption');
 const modalPhotoCommentsCount = modalPhoto.querySelector('.comments-count');
 const modalPhotoCommentsList = modalPhoto.querySelector('.social__comments');
 const modalButtonCancel = modalPhoto.querySelector('.big-picture__cancel');
+const commentsLoaderButton =  modalPhoto.querySelector('.social__comments-loader');
+const COMMENTS_MIN_COUNT = 5;
+let numberDisplayedComments = COMMENTS_MIN_COUNT;
 
 const onPopUpEscKeydown = (evt) => {
   if (isEscEvent(evt)) {
@@ -14,25 +17,49 @@ const onPopUpEscKeydown = (evt) => {
   }
 }
 
+const cleanCommentaries = () => {
+  const comment = modalPhotoCommentsList.querySelectorAll('.social__comment');
+  comment.forEach((element) => {
+    element.remove();
+  });
+}
+
 const closeModal = () => {
   modalPhoto.classList.add('hidden');
   document.removeEventListener('keydown',onPopUpEscKeydown);
   modalPhoto.querySelector('.social__comment-count').classList.remove('hidden');
-  modalPhoto.querySelector('.social__comments-loader').classList.remove('hidden');
   document.querySelector('body').classList.remove('modal-open');
+  modalButtonCancel.removeEventListener('click', closeModal);
+  closeCommentariesButton();
+  cleanCommentaries();
 }
 
 const openModal = () => {
   modalPhoto.classList.remove('hidden');
   document.addEventListener('keydown',onPopUpEscKeydown);
   modalPhoto.querySelector('.social__comment-count').classList.add('hidden');
-  modalPhoto.querySelector('.social__comments-loader').classList.add('hidden');
   document.querySelector('body').classList.add('modal-open');
+  modalButtonCancel.addEventListener('click', closeModal);
+}
+
+const openCommentariesButton = (arrayComments) => {
+  commentsLoaderButton.classList.remove('hidden');
+
+  commentsLoaderButton.addEventListener('click', () => {
+    cleanCommentaries();
+    numberDisplayedComments += 5;
+    renderCommentaries(arrayComments);
+  });
+}
+
+const closeCommentariesButton = () => {
+  commentsLoaderButton.classList.add('hidden');
+  numberDisplayedComments = COMMENTS_MIN_COUNT;
+  commentsLoaderButton.removeEventListener('click', () => {}); // Заготовка
 }
 
 const renderModalPicture = (object) => {
   openModal();
-
   modalPhotoImg.src = object.url;
   modalPhotoImg.alt = '';
   modalPhotoLikes.textContent = String(object.likes);
@@ -41,19 +68,25 @@ const renderModalPicture = (object) => {
   modalPhotoCommentsList.innerHTML = '';
 
   if (object.comments.length > 0) {
-    object.comments.forEach((element) => {
-      const item = document.createElement('li');
-      item.classList.add('social__comment');
-      item.innerHTML = `<img class="social__picture" src="${element.avatar}" alt="${element.name}" width="35" height="35">
-      <p class="social__text">${element.message}</p>`;
-      modalPhotoCommentsList.appendChild(item);
-    });
+    renderCommentaries(object.comments);
   }
+  if (object.comments.length > COMMENTS_MIN_COUNT) {
+    openCommentariesButton(object.comments);
+  }
+}
 
-  modalButtonCancel.addEventListener('click', () =>{
-    closeModal();
+const renderCommentaries = (arrayComments) => {
+  arrayComments.slice(0, numberDisplayedComments).forEach((element) => {
+    const item = document.createElement('li');
+    item.classList.add('social__comment');
+    item.innerHTML = `<img class="social__picture" src="${element.avatar}" alt="${element.name}" width="35" height="35">
+      <p class="social__text">${element.message}</p>`;
+    modalPhotoCommentsList.appendChild(item);
+
+    if (modalPhotoCommentsList.querySelectorAll('.social__comment').length === arrayComments.length ) {
+      closeCommentariesButton();
+    }
   });
-
 }
 
 export {renderModalPicture};
