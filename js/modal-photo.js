ddc6bec5fd1ebdc5dd9706ff9f1a44ee42372676
DanteDeGrave <vladimir.system@gmail.com>
2021-03-03 14:53:26 +0500

    Сделал слушателя именным и реализовал его удаление, так же добавил счетчик комментариев

--- js/modal-photo.js
+++ js/modal-photo.js
@@ -8,8 +8,10 @@ const modalPhotoCommentsCount = modalPhoto.querySelector('.comments-count');
 const modalPhotoCommentsList = modalPhoto.querySelector('.social__comments');
 const modalButtonCancel = modalPhoto.querySelector('.big-picture__cancel');
 const commentsLoaderButton =  modalPhoto.querySelector('.social__comments-loader');
+const commentCount = modalPhoto.querySelector('.social__comment-count');
 const COMMENTS_MIN_COUNT = 5;
 let numberDisplayedComments = COMMENTS_MIN_COUNT;
+let commentaries = [];
 
 const onPopUpEscKeydown = (evt) => {
   if (isEscEvent(evt)) {
@@ -27,35 +29,36 @@ const cleanCommentaries = () => {
 const closeModal = () => {
   modalPhoto.classList.add('hidden');
   document.removeEventListener('keydown',onPopUpEscKeydown);
-  modalPhoto.querySelector('.social__comment-count').classList.remove('hidden');
   document.querySelector('body').classList.remove('modal-open');
   modalButtonCancel.removeEventListener('click', closeModal);
   closeCommentariesButton();
   cleanCommentaries();
+  commentsLoaderButton.removeEventListener('click', switchShowMoreButton);
+  numberDisplayedComments = COMMENTS_MIN_COUNT;
 }
 
 const openModal = () => {
   modalPhoto.classList.remove('hidden');
   document.addEventListener('keydown',onPopUpEscKeydown);
-  modalPhoto.querySelector('.social__comment-count').classList.add('hidden');
+  commentCount.classList.add('hidden');
   document.querySelector('body').classList.add('modal-open');
   modalButtonCancel.addEventListener('click', closeModal);
 }
 
-const openCommentariesButton = (arrayComments) => {
-  commentsLoaderButton.classList.remove('hidden');
+const switchShowMoreButton = () => {
+  cleanCommentaries();
+  numberDisplayedComments += 5;
+  renderCommentaries();
+  commentCount.innerHTML = `${modalPhotoCommentsList.children.length} из <span class="comments-count">${commentaries.length}</span> комментариев`;
+}
 
-  commentsLoaderButton.addEventListener('click', () => {
-    cleanCommentaries();
-    numberDisplayedComments += 5;
-    renderCommentaries(arrayComments);
-  });
+const openCommentariesButton = () => {
+  commentsLoaderButton.classList.remove('hidden');
+  commentsLoaderButton.addEventListener('click', switchShowMoreButton);
 }
 
 const closeCommentariesButton = () => {
   commentsLoaderButton.classList.add('hidden');
-  numberDisplayedComments = COMMENTS_MIN_COUNT;
-  commentsLoaderButton.removeEventListener('click', () => {}); // Заготовка
 }
 
 const renderModalPicture = (object) => {
@@ -63,27 +66,27 @@ const renderModalPicture = (object) => {
   modalPhotoImg.src = object.url;
   modalPhotoImg.alt = '';
   modalPhotoLikes.textContent = String(object.likes);
-  modalPhotoCommentsCount.textContent = String(object.comments.length);
   modalPhotoDescription.textContent = object.description;
   modalPhotoCommentsList.innerHTML = '';
-
-  if (object.comments.length > 0) {
-    renderCommentaries(object.comments);
+  commentaries = object.comments;
+  if (commentaries.length > 0) {
+    renderCommentaries();
   }
-  if (object.comments.length > COMMENTS_MIN_COUNT) {
-    openCommentariesButton(object.comments);
+  if (commentaries.length > COMMENTS_MIN_COUNT) {
+    openCommentariesButton();
+    commentCount.classList.remove('hidden');
   }
+  commentCount.innerHTML = `${modalPhotoCommentsList.children.length} из <span class="comments-count">${commentaries.length}</span> комментариев`;
 }
 
-const renderCommentaries = (arrayComments) => {
-  arrayComments.slice(0, numberDisplayedComments).forEach((element) => {
+const renderCommentaries = () => {
+  commentaries.slice(0, numberDisplayedComments).forEach((element) => {
     const item = document.createElement('li');
     item.classList.add('social__comment');
     item.innerHTML = `<img class="social__picture" src="${element.avatar}" alt="${element.name}" width="35" height="35">
       <p class="social__text">${element.message}</p>`;
     modalPhotoCommentsList.appendChild(item);
-
-    if (modalPhotoCommentsList.querySelectorAll('.social__comment').length === arrayComments.length ) {
+    if (modalPhotoCommentsList.querySelectorAll('.social__comment').length === commentaries.length ) {
       closeCommentariesButton();
     }
   });
